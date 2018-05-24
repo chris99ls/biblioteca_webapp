8fc9d4ba28be17047ac6a3b7e998a2c374008024
Christian <chris99ls.cl@gmail.com>
2018-05-24 17:23:44 +0200

    aggiunta scheletro home page html

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Libro
+from .models import Libro , Sinossi, Prestito, Gestore, Utenti
 # Register your models here.
 
 
 admin.site.register(Libro)
+admin.site.register(Sinossi)
+admin.site.register(Prestito)
+admin.site.register(Gestore)
+admin.site.register(Utenti)
