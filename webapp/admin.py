f57c8c68b03d7911a583c6c202926231ae34f15f
Christian <chris99ls.cl@gmail.com>
2018-05-27 18:28:09 +0200

    models adjustments

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from .models import Libro , Sinossi, Prestito, Gestore, Utenti
+from .models import Libro , Sinossi, Prestito
 # Register your models here.
 
 
 admin.site.register(Libro)
 admin.site.register(Sinossi)
 admin.site.register(Prestito)
-admin.site.register(Gestore)
-admin.site.register(Utenti)
--- webapp/models.py
+++ webapp/models.py
@@ -39,38 +39,13 @@ class Sinossi(models.Model):
 class Prestito(models.Model):
 
     id=models.AutoField(primary_key='True')
-    n_tessera= models.ForeignKey('Utenti', on_delete=models.CASCADE)
+    n_tessera= models.ForeignKey('auth.User', on_delete=models.CASCADE)
     isbn= models.BigIntegerField()
     start_loan_date = models.DateTimeField(default=timezone.now)
     end_loan_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.n_tessera
-
-    def publish(self):
-        self.save()
-
-class Utenti(models.Model):
-
-    nome=models.CharField(max_length=200)
-    cognome=models.CharField(max_length=200)
-    n_tessera=models.AutoField(primary_key='True')
-    classe=models.CharField(max_length=200)
-    
-    def __str__(self):
-        return self.n_tessera
-
-    def publish(self):
-        self.save()
-
-
-class Gestore(models.Model):
-    id= models.AutoField(primary_key='True')
-    nome=models.CharField(max_length=200)
-    cognome=models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.nome
 
     def publish(self):
         self.save()
